0a6152802676c14517a29991799a46f77b1668f5
nikitazapekin <zapekinnikitamain@gmail.com>
2025-09-26 19:11:05 +0300

    feat(token): Update ci cd token

--- src/utils/helpers/extractHours/extractHours.ts
+++
@@ -1,7 +0,0 @@
-export function extractTime(dateTimeString: string): string {
-  const date = new Date(dateTimeString);
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-
-  return `${hours}:${minutes.toString().padStart(2, "0")}`;
-}
--- src/utils/helpers/isValidCacheKey.ts/isValidCacheKey.ts
+++
@@ -1,5 +0,0 @@
-import type { WeatherCacheKey } from "@types/cachedResponse";
-
-export function isWeatherCacheKey(key: string): key is WeatherCacheKey {
-  return key.startsWith("weather_cache_");
-}
