f1301e1b10a4e7b9f57aecfe16c029f199c551fb
nikitazapekin <zapekinnikitamain@gmail.com>
2025-10-02 17:05:03 +0300

    feat(scroll): Add horizontal scroll

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,16 +1,13 @@
- 
- 
 import { Board } from "@components/Board";
 import { GlobalStyle, PageWrapper, Reset, WrapperContainer } from "@styles";
- 
+
 const WeatherPage = () => {
- 
   return (
     <PageWrapper>
       <WrapperContainer>
         <GlobalStyle />
         <Reset />
-     <Board />
+        <Board />
       </WrapperContainer>
     </PageWrapper>
   );
